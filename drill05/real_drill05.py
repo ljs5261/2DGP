37465f1918f5279d50008153a361fc67d05941b9
deigonz5261 <ljs5261@gmail.com>
2018-09-18 21:25:39 +0900

    좌표(203, 535)를 point1으로 하고 그 위치에 캐릭터를 그리는 함수를 호출합니다

--- drill05/real_drill05.py
+++ drill05/real_drill05.py
@@ -7,40 +7,48 @@ character = load_image('character.png')
 
 # fill here
 
-def move_from_point1_to_point2():
-    pass
 
 def move_from_point1_to_point2():
-    pass
+    x, y = 203, 535
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(5)
+
 
 def move_from_point2_to_point3():
     pass
 
+
 def move_from_point3_to_point4():
     pass
+
 
 def move_from_point4_to_point5():
     pass
 
+
 def move_from_point5_to_point6():
     pass
+
 
 def move_from_point6_to_point7():
     pass
 
+
 def move_from_point7_to_point8():
     pass
+
 
 def move_from_point8_to_point9():
     pass
 
+
 def move_from_point9_to_point10():
     pass
 
+
 def move_from_point10_to_point1():
     pass
-
-
 
 
 def move_loop():
@@ -54,5 +62,10 @@ def move_loop():
     move_from_point8_to_point9()
     move_from_point9_to_point10()
     move_from_point10_to_point1()
+
+    pass
+
+
+move_from_point1_to_point2()
 
 close_canvas()
