46a6df12de67fafc90ea186d8ac7000614e1e54a
deigonz5261 <ljs5261@gmail.com>
2018-09-20 20:37:33 +0900

    handle_events 논리식 수정, 배경 이미지 좌표값 따로 변수처리

--- drill06/drill06.py
+++ drill06/drill06.py
@@ -13,7 +13,7 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == SDL_BUTTON_LEFT:
                 x, y = event.x, KPU_HEIGHT - 1 - event.y
-        elif event.type == SDL_MOUSEBUTTONUP and event.key == SDLK_ESCAPE:
+        elif event.key == SDLK_ESCAPE:
             running = False
 
 
@@ -23,15 +23,16 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+ground_x, ground_y = KPU_WIDTH // 2, KPU_HEIGHT // 2            # kpu 배경이미지 좌표
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2            # 캐릭터의 최초 좌표
 frame = 0
 
 while running:
     clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    kpu_ground.draw(ground_x, ground_y)
     character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
-    handle_events()
+    handle_events()        # 마우스클릭 좌표값으로 x, y를 갱신
     frame = (frame + 1) % 8
 
     delay(0.05)
